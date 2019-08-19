79ab0db9f2076c6f3cf1a59fbe29dd8f4cb354c8
EduardoHi <eshidalgo97@gmail.com>
2019-08-18 21:56:29 -0500

    Polybar setup for multi-monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,5 +7,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch our bars...
-polybar bottom &
-
+# old way: polybar bottom &
+# to launch the same bar to each monitor
+# "bottom is the name of the bar in the config
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bottom &
+done
